7f32be42d1ff602ee67d704a60632980918a0ca0
zhangjg <jinguo_zhang@matrixdigi.com>
2014-06-24 18:36:44 +0800

    begian lambda

--- RCL.js
+++ RCL.js
@@ -1,82 +1,79 @@
 Readline=require('readline');
 var environment={};
 
+environment.bye=function()
+{
+    console.log("bye");
+    process.exit(0);
+}
+
 environment['+']=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-    var ret=argument[1];
-    for(var i=2;i<argument.length;++i)
+    //console.trace(arguments);
+    var ret=arguments[1];
+    for(var i=2;i<arguments.length;++i)
     {
-        ret += argument[i];
+        ret += arguments[i];
     }
     return ret;
 };
+
 
 environment['-']=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-    var ret=argument[1];
-    for(var i=2;i<argument.length;++i)
+    //check(arguments);
+    var ret=arguments[1];
+    for(var i=2;i<arguments.length;++i)
     {
-        ret -= argument[i];
+        ret -= arguments[i];
     }
     return ret;
 };
 
+
 environment['*']=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-    var ret=argument[1];
-    for(var i=2;i<argument.length;++i)
+    //check(arguments);
+    var ret=arguments[1];
+    for(var i=2;i<arguments.length;++i)
     {
-        ret *= argument[i];
+        ret *= arguments[i];
     }
     return ret;
 };
 
 environment['/']=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-    var ret=argument[1];
-    for(var i=2;i<argument.length;++i)
+    var ret=arguments[1];
+    for(var i=2;i<arguments.length;++i)
     {
-        ret /= argument[i];
+        ret /= arguments[i];
     }
     return ret;
 }
 
+
 environment.quote=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-    if(argument.length !=2 )
+    //console.log("arguments:%j",arguments);
+    if(arguments.length <2 )
     {
         console.error('bad synty');
+        throw new Error("bad synty");
     }
-    return argument[2];
-}
+    return arguments[1];
+};
 
 environment.autom=function()
 {
-    if(argument[0]!=argument.caller.name){
-        throw Error("Parse Error");
-    }
-
-    if(argument.length!= 2)
-    {       
+    //console.log("arguments:%j",arguments);
+    if(arguments.length!= 2)
+    {
         console.error('bad synty');
+        throw new Error("bad synty");
     }
 
-    var x=argument[1];
+    var x=arguments[1];
     if( x instanceof Array)
     {
         if( x.length==0)
@@ -90,36 +87,82 @@ environment.autom=function()
     {
         return true;
     }
-}
+};
 
 environment.eq=function()
 {
-    if(argument.caller.name==argument[0])
+    //check(arguments);
+    if(arguments.length != 3)
     {
-
+        throw new Error("bad synty");
     }
-    return a === b ;
-}
+    return arguments[1] === arguments[2] ;
+};
 
-environment.car=function(l)
-{
-    return l[0];
-}
+environment['=?']=environment.eq;
 
-environment.cdr=function(l)
-{
-    return l.slice(1)
-}
 
-environment.cons=function(a,l)
+environment.car=function()
 {
-    if(this.autom(l))
+    //console.log("arguments:%j",arguments);
+    if(arguments.length == 2)
     {
-        return [a,l];
+        var l = arguments[1];
+        if(l instanceof Array)
+        {
+             return l[0];
+        }
     }
-    var t=[a];
-    t.concat(l);
-}
+    throw new Error("bad synty");
+};
+
+
+environment.cdr=function()
+{
+    //check(arguments);
+    if(arguments.length == 2)
+    {
+        var l = arguments[1];
+        if(l instanceof Array)
+        {
+            return l.slice(1);
+        }
+    }
+    throw new Error("bad synty");
+};
+
+//(lamda (a1,a3)())
+environment.lambda=function(lambda,param,body)
+{
+    console.log('arguments:%j',arguments);
+    var env={};
+    for(var i=0;param.length;++i)
+    {
+        env[param[i]]=null;
+    }
+    var ret=[];
+    ret.concat(ret);
+    ret.env=env;
+    return ret;
+};
+
+environment.cons=function()
+{
+    //check(arguments)
+    console.log("arugments:%j",arguments);
+    if(arguments.length != 3)
+    {
+        throw new Error("bad synty");
+    }
+    var t=arguments[2];
+    if(t instanceof Array){
+        t.unshift(arguments[1]);
+    }else{
+        t=[arguments[1],arguments[2]];
+    }
+    return t;
+};
+
 
 function value(l,env)
 {
@@ -128,7 +171,39 @@ function value(l,env)
     {
         env.prototype=environment;
     }
-env[l[0]]
+    if(l instanceof Array == false)
+    {
+        return l;
+    }else{
+        if(l[0] == 'quote')
+        {
+            return env.quote.apply(env,l);
+        }
+        if(l[0] == 'lambda')
+        {
+            return env.lambda.apply(env,l);
+        }
+        l.forEach(function (item,index,array)
+        {
+            if(index != 0)
+            {
+                if(item instanceof Array)
+                { 
+                    var result=value(item,env);
+                    //console.log("item:%j=%j",item,result);
+                    array[index]=result;
+                }
+            }
+        });
+        
+        if(l[0] in env)
+        {
+            return env[l[0]].apply(env,l);
+        }else
+        {
+            throw new Error("Not not find function "+l[0]);
+        }
+    }
 }
 
 
@@ -155,8 +230,6 @@ function Parser(src)
             {
                 obj[count-2].push(obj[count-1]);
             }
-            console.log("count:%d",count);
-            console.log(obj[0]);
         }else if(src[i]==')')
         {
             ele=ele.trim();
@@ -178,9 +251,41 @@ function Parser(src)
             ele+=src[i];
         }
     }
+    
+    function s_t_n(str)
+    {
+        var i=/[\+-]?(\d+|0x[\da-fA-F]+|0[0-7]+)/;
+        var d=/^((\+|-)?\d*\.?\d+)$/;
+        var d2=/^((\+|-)?\d*\.?\d+(e|E)(\+|-)\d+)$/
+       
+        if(i.test(str) || d.test(str) ||d2.test(str))
+        {
+            return Number(str);
+        }
+        return str;
+    }
     if(count ==0 )
     {
-        return obj[count];
+        if((count in obj )== false && ele!='')
+        {
+            return s_t_n(ele)
+        }
+        var ret = obj[count];
+        //console.log(ret);
+        ret= ret.map(function (item){
+            if(item instanceof Array)
+            {
+                //return item.map(arguments.caller);
+                //console.log(arguments.callee.name);
+                return item.map(arguments.callee);
+            }
+            if( typeof(item) == "string")
+            {
+                return s_t_n(item);
+            }
+        });
+        //console.log(ret);
+        return ret
     }else
     {
         throw new Error("Not a lisp");
@@ -189,24 +294,29 @@ function Parser(src)
 
 function Computer(str)
 {
-    console.log(Parser(str));
+    if(str.trim() == '')
+    {
+        console.log(str);
+        return;
+    }
+    var l = Parser(str);
+    var t=value( l );
+    if(t instanceof Array)
+    {
+        t=t.join(" ");
+        t="("+t+")";
+    }
+    console.log( t );
 }
+
 function Loop(rl)
 {
     rl.prompt();
     rl.on('line',function(line){
         line=line.trim();
         rl.prompt();
-        if(line=='quit' || line=='exit'||line=='bye' )
-        {
-            console.log("bye");
-            rl.close();
-            return ;
-        }else
-        {
-            Computer(line);
-            rl.prompt();
-        }
+        Computer(line);
+        rl.prompt();
     });
 }
 
